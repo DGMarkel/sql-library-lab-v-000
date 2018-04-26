b83a746c670388e6c27862bdd1a19c0beb19738d
David Markel <davidgmarkel@gmail.com>
2018-04-26 18:43:17 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,12 +32,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  SELECT series.title 
+  "SELECT series.title 
   FROM series
   JOIN characters
   ON series.id = characters.series_id
   GROUP BY species
-  ORDER BY COUNT(species) DESC LIMIT 1
+  ORDER BY COUNT(species) DESC LIMIT 1;
   "
 end
 
